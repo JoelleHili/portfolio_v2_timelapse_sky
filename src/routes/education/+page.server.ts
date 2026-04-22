700829140550f9e2728b492dad1d8f30c03d02a1
Joelle Hili <joelle.hili@outlook.com>
2026-04-22 19:50:46 +0200

    feat: Added Education Page

---
+++ src/routes/education/+page.server.ts
@@ -0,0 +1,5 @@
+import { educationData } from '../../data/educationData.ts'
+
+export function load() {
+    return {educationData};
+}
---
+++ src/routes/education/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	let { data } = $props();
+
+	$effect(() => {
+		const popover = document.getElementById('navigation-popover');
+		popover?.hidePopover();
+
+		document.body.dataset.theme = 'Evening';
+	});
+</script>
+
+<title>My Education</title>
+
+{#each data.educationData as edu}
+	<section class="education__entry">
+		<section class="education__entry__title">
+			<img class="education__entry__icon" src={`/src/lib/${edu.icon}.svg`} alt={edu.icon} height="24px" width="24px" />
+			<h2>{edu.title}</h2>
+		</section>
+		<p class="education__entry__info">
+			{edu.college}, {#if edu.campus}{edu.campus},
+			{/if}{edu.dateRange}
+		</p>
+	</section>
+{/each}
+
+<style>
+
+    .education__entry{
+        margin-right: 10dvw;
+        display: block;
+        padding-bottom: 16px;
+    }
+
+    .education__entry__title{
+        display: flex;
+        flex-direction: row;
+        gap: 16px;
+        align-items: center;
+    }
+
+    .education__entry__info{
+        margin-left: calc(var(--icon-size) + 16px);
+        margin-top: 4px;
+    }
+
+    .education__entry__icon{
+        height: var(--icon-size);
+        width: var(--icon-size);
+
+        color: #f1f7ee;
+    }
+
+    :root{
+        --icon-size: clamp(24px, 3dvw , 48px);
+    }
+</style>
