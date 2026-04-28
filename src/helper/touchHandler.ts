3374babc9cce766f207eddc8fd6d7d1aabc91d17
Joelle Hili <joelle.hili@outlook.com>
2026-04-28 11:48:18 +0200

    fix: More Touch Filter Adjustments

--- src/helper/touchHandler.ts
+++ src/helper/touchHandler.ts
@@ -1,7 +1,7 @@
 export const getGestureType = (touchStartY?: number, touchEndY?: number) => {
 	if (touchStartY && touchEndY) {
 		const touchDistance = touchStartY - touchEndY;
-		const touchFilter = 150;
+		const touchFilter = 100;
 
 		if (touchEndY < touchStartY && Math.abs(touchDistance) > touchFilter) return 'swipeUp';
 		else if (touchEndY > touchStartY && Math.abs(touchDistance) > touchFilter) return 'swipeDown';
